51324b1b438f15384e2615cd4e8034a838241651
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-05-25 15:45:28 +0530

    Working with admin dashboard layout

--- CareSync.Core/Entities/ApplicationUser.cs
+++ CareSync.Core/Entities/ApplicationUser.cs
@@ -3,6 +3,8 @@ using CareSync.Core.Entities;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
+    public string FullName { get; set; }
+    public string Role { get; set; }
     public virtual ICollection<Doctor> Doctors { get; set; }
     public virtual ICollection<Patient> Patients { get; set; }
     public virtual ICollection<Notification> Notifications { get; set; }
